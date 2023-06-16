cb217b462ea94225873b65db925d62dd2ad3757b
PabloAparicioMartinez <pablo.aparicio1996@gmail.com>
2023-06-16 11:31:02 +0200

    25. Rama Pupum: solo he puesto // en FragmentMyTickets 17-21.

--- app/src/main/java/com/example/nq/FragmentMyTickets.kt
+++ app/src/main/java/com/example/nq/FragmentMyTickets.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.nq.authSignIn.SignInActivity
-import com.example.nq.zzOldFirebase.FirebaseManager
+//import com.example.nq.zzOldFirebase.FirebaseManager
 import kotlinx.android.synthetic.main.fragment_my_tickets.*
 
 class FragmentMyTickets : Fragment(R.layout.fragment_my_tickets) {
@@ -14,11 +14,11 @@ class FragmentMyTickets : Fragment(R.layout.fragment_my_tickets) {
         super.onViewCreated(view, savedInstanceState)
 
         //SET LAYOUT DEPENDING ON USER LOGGED IN STATE
-        if (!FirebaseManager().checkIfUserIsSignedIn()) {
-            setLayoutVisibilities(listOf(View.GONE, View.VISIBLE))
-        } else {
-            setLayoutVisibilities(listOf(View.VISIBLE, View.GONE))
-        }
+//        if (!FirebaseManager().checkIfUserIsSignedIn()) {
+//            setLayoutVisibilities(listOf(View.GONE, View.VISIBLE))
+//        } else {
+//            setLayoutVisibilities(listOf(View.VISIBLE, View.GONE))
+//        }
 
         //BUTTONS
 //        fragMyTickets_buyTicketsSignedInButton.setOnClickListener {
--- app/src/main/java/com/example/nq/zzOldFirebase/FirebaseUserData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nq.zzOldFirebase
-
-import android.net.Uri
-
-class FirebaseUserData(
-    name: String,
-    uri: Uri,
-    mail: String
-)
